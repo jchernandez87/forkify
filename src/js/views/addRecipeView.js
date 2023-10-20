b32236eb3981c4e8f79d50aa2412a9dd6ac6c1e4
Juan Carlos <jchernandez827@gmail.com>
2023-10-20 13:52:44 -0500

    Create and setup AddRecipeView

---
+++ src/js/views/addRecipeView.js
@@ -0,0 +1,68 @@
+import View from './View.js';
+import icons from 'url:../../img/icons.svg';
+
+class AddRecipeView extends View {
+  _parentElement = document.querySelector('.pagination');
+
+  addHandlerClick(handler) {
+    this._parentElement.addEventListener('click', e => {
+      const btn = e.target.closest('.btn--inline');
+      if (!btn) return;
+
+      const goToPage = +btn.dataset.goto;
+
+      handler(goToPage);
+    });
+  }
+
+  _generatePrevBtn(page) {
+    return `
+      <button data-goto="${page - 1}" class="btn--inline pagination__btn--prev">
+        <svg class="search__icon">
+          <use href="${icons}#icon-arrow-left"></use>
+        </svg>
+        <span>Page ${page - 1}</span>
+      </button>
+    `;
+  }
+
+  _generateNextBtn(page) {
+    return `
+        <button data-goto="${
+          page + 1
+        }" class="btn--inline pagination__btn--next">
+          <span>Page ${page + 1}</span>
+          <svg class="search__icon">
+            <use href="${icons}#icon-arrow-right"></use>
+          </svg>
+        </button>
+      `;
+  }
+
+  _generateMarkup() {
+    const currPage = this._data.page;
+
+    const numPages = Math.ceil(
+      this._data.results.length / this._data.resultsPerPage
+    );
+
+    if (currPage === 1 && numPages > 1) {
+      return `${this._generateNextBtn(currPage)};`;
+    }
+
+    if (currPage === numPages && numPages > 1) {
+      return `${this._generatePrevBtn(currPage)}`;
+    }
+
+    if (currPage < numPages) {
+      return `
+        ${this._generatePrevBtn(currPage)}
+        ${this._generateNextBtn(currPage)}
+      `;
+    }
+
+    return '';
+  }
+}
+
+export default new AddRecipeView();
